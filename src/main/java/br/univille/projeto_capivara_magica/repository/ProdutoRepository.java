3f83491f21d725f604787a7989dd9128b783deb8
Phrdavis <davipi_sou@hotmail.com>
2024-05-13 22:09:54 -0300

    Novos Endpoints
    Adicionando novos endpoints para acessar informações do usuario

--- src/main/java/br/univille/projeto_capivara_magica/repository/ProdutoRepository.java
+++ src/main/java/br/univille/projeto_capivara_magica/repository/ProdutoRepository.java
@@ -1,9 +1,6 @@
 package br.univille.projeto_capivara_magica.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.univille.projeto_capivara_magica.entity.Produto;
